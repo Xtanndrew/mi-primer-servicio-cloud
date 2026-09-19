f321a009033a6501b03b826215cbff3bb695fcd5
Andrés Ramírez Quezada <itic.andres@gmail.com>
2026-09-19 12:53:16 -0600

    Conectar productos con Google Sheets Netlify

--- netlify/functions/productos.mjs
+++ netlify/functions/productos.mjs
@@ -1,20 +1,57 @@
 import { google } from "googleapis";
 
+const auth = new google.auth.JWT(
+    process.env.GOOGLE_CLIENT_EMAIL,
+    null,
+    process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    [
+        "https://www.googleapis.com/auth/spreadsheets.readonly"
+    ]
+);
+
+const sheets = google.sheets({
+    version: "v4",
+    auth
+});
+//conexión a google sheets
+//comentario para actualizar
+// $$$$$
 export default async () => {
 
-    return Response.json({
-        clientEmailExiste: !!process.env.GOOGLE_CLIENT_EMAIL,
-        privateKeyExiste: !!process.env.GOOGLE_PRIVATE_KEY,
-        sheetIdExiste: !!process.env.GOOGLE_SHEET_ID,
+    try {
 
-        clientEmailInicio:
-            process.env.GOOGLE_CLIENT_EMAIL
-                ? process.env.GOOGLE_CLIENT_EMAIL.substring(0, 10) + "..."
-                : null,
+        const result = await sheets.spreadsheets.values.get({
+            spreadsheetId: process.env.GOOGLE_SHEET_ID,
+            range: "Productos!A2:D"
+        });
 
-        privateKeyInicio:
-            process.env.GOOGLE_PRIVATE_KEY
-                ? process.env.GOOGLE_PRIVATE_KEY.substring(0, 30) + "..."
-                : null
-    });
+        const rows = result.data.values || [];
+
+        const productos = rows.map((row) => ({
+            id: row[0] || "",
+            nombre: row[1] || "",
+            precio: Number(row[2] || 0),
+            categoria: row[3] || ""
+        }));
+// camnbios para actualizar
+        return Response.json(productos);
+
+    } catch (error) {
+
+        //comentarios
+        console.error(
+            "Error al consultar Google Sheets!:",
+            error
+        );
+
+        return Response.json(
+            {
+                error: "No fue posible consultar Google Sheets!",
+                detalle: error.message
+            },
+            {
+                status: 500
+            }
+        );
+    }
 };
